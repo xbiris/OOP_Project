13f68f58fc6760699981bc4754120203ad7f7d96
David BIris <david.biris1@stud.ubbcluj.ro>
2022-05-07 14:44:49 +0300

    commit1

---
+++ cmake-build-debug/CMakeFiles/OopProject.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/Facultate/OOP/OopProject/Domain.cpp" "/mnt/d/Facultate/OOP/OopProject/cmake-build-debug/CMakeFiles/OopProject.dir/Domain.cpp.o"
+  "/mnt/d/Facultate/OOP/OopProject/Repository.cpp" "/mnt/d/Facultate/OOP/OopProject/cmake-build-debug/CMakeFiles/OopProject.dir/Repository.cpp.o"
+  "/mnt/d/Facultate/OOP/OopProject/main.cpp" "/mnt/d/Facultate/OOP/OopProject/cmake-build-debug/CMakeFiles/OopProject.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OopProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OopProject.dir/Domain.cpp.o"
+  "CMakeFiles/OopProject.dir/Repository.cpp.o"
+  "CMakeFiles/OopProject.dir/main.cpp.o"
+  "OopProject"
+  "OopProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OopProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
